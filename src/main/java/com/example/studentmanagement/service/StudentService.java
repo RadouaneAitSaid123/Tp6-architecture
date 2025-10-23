4dbb6cc7ae62c7617bd928413c6b25a41542b544
Radouane ait said <radouane.aitsaid01@gmail.com>
2025-10-24 00:43:43 +0100

    first commit

---
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -0,0 +1,49 @@
+package com.example.studentmanagement.service;
+
+import com.example.studentmanagement.entity.Student;
+import com.example.studentmanagement.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author radouane
+ **/
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student save(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public boolean delete(int id) {
+        Optional<Student> studentOptional = Optional.ofNullable(studentRepository.findById(id));
+        if (studentOptional.isPresent()) {
+            studentRepository.delete(studentOptional.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public long countStudents() {
+        return studentRepository.count();
+    }
+
+    public Collection<?> findNbrStudentByYear() {
+        return studentRepository.findNbrStudentByYear();
+    }
+}
